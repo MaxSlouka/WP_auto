59367516c3676d7257a985c4945a24b079bc6160
MaxSlouka <jmslouka@gmail.com>
2023-09-24 16:56:40 +0200

    create get all methods

--- backend/auto_project/src/main/java/services/implementation/CarServiceImpl.java
+++ backend/auto_project/src/main/java/services/implementation/CarServiceImpl.java
@@ -1,6 +1,13 @@
 package services.implementation;
 
+import repo.CarRepository;
 import services.CarService;
 
 public class CarServiceImpl implements CarService {
+    private CarRepository carRepository;
+
+    @Override
+    public Object getAll() {
+        return carRepository.findAll();
+    }
 }

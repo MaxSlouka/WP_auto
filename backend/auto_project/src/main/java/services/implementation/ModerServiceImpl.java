444dbecc543be17046ab23a0d87b5d440d50f48c
MaxSlouka <jmslouka@gmail.com>
2023-09-24 16:56:57 +0200

    create get all methods

--- backend/auto_project/src/main/java/services/implementation/ModerServiceImpl.java
+++ backend/auto_project/src/main/java/services/implementation/ModerServiceImpl.java
@@ -1,6 +1,13 @@
 package services.implementation;
 
+import repo.ModelRepository;
 import services.ModelService;
 
 public class ModerServiceImpl implements ModelService {
+
+    private ModelRepository modelRepository;
+    @Override
+    public Object getAll() {
+        return modelRepository.findAll();
+    }
 }

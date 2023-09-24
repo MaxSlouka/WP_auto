4be4517ca37c1eb3c0dff6de96776faa347f4294
MaxSlouka <jmslouka@gmail.com>
2023-09-24 16:29:51 +0200

    create model repo

---
+++ backend/auto_project/src/main/java/repo/ModelRepository.java
@@ -0,0 +1,9 @@
+package repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import auto.backe.auto_project.models.Model;
+
+@Repository
+public interface ModelRepository extends JpaRepository<Model, Long> {
+}

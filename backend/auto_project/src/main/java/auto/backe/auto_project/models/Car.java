294f0926bb046ddc409bb8deddf610f89fa5007e
MaxSlouka <jmslouka@gmail.com>
2023-09-24 16:04:49 +0200

    add property fields

--- backend/auto_project/src/main/java/auto/backe/auto_project/models/Car.java
+++ backend/auto_project/src/main/java/auto/backe/auto_project/models/Car.java
@@ -1,4 +1,19 @@
 package auto.backe.auto_project.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Car {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+    private Long modelId;
+    private String color;
+    private int performance;
+    private int consumption;
+    private int yearOfProduction;
+    private boolean isActive;
+
 }

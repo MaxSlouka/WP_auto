96e1d360b7336a8dd9984ec046f2de4841364396
MaxSlouka <jmslouka@gmail.com>
2023-09-24 15:15:48 +0200

    create new project

---
+++ backend/auto_project/src/test/java/auto/backe/auto_project/AutoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package auto.backe.auto_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

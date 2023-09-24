f4a0a0e596535991e55ccbeda482cfed8d711332
MaxSlouka <jmslouka@gmail.com>
2023-09-24 16:56:48 +0200

    create get all methods

--- backend/auto_project/src/main/java/services/implementation/ManufacturerServiceImpl.java
+++ backend/auto_project/src/main/java/services/implementation/ManufacturerServiceImpl.java
@@ -1,6 +1,13 @@
 package services.implementation;
 
+import repo.ManufacturerRepository;
 import services.ManufacturerService;
 
 public class ManufacturerServiceImpl implements ManufacturerService {
+
+    private ManufacturerRepository manufacturerRepository;
+    @Override
+    public Object getAll() {
+        return manufacturerRepository.findAll();
+    }
 }

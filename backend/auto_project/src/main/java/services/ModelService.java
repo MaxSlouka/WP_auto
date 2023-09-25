a3612d09899c0f923ab1d581ca5430f49e1de432
MaxSlouka <jmslouka@gmail.com>
2023-09-25 14:26:27 +0200

    create delete and find by id for model service

--- backend/auto_project/src/main/java/services/ModelService.java
+++ backend/auto_project/src/main/java/services/ModelService.java
@@ -1,8 +1,15 @@
 package services;
 
+import auto.backe.auto_project.models.Model;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public interface ModelService {
     Object getAll();
+
+    Optional<Model> findById(Long id);
+
+    void deleteModel(Model model);
 }

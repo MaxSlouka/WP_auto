097e33e88dd9cd3f520ffe6a75ec3131557878a4
MaxSlouka <jmslouka@gmail.com>
2023-09-25 16:02:10 +0200

    add lombok

--- backend/auto_project/src/main/java/auto/backe/auto_project/models/Car.java
+++ backend/auto_project/src/main/java/auto/backe/auto_project/models/Car.java
@@ -3,8 +3,16 @@ package auto.backe.auto_project.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Car {
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
@@ -16,4 +24,7 @@ public class Car {
     private int yearOfProduction;
     private boolean isActive;
 
+    public boolean getIsActive() {
+        return isActive;
+    }
 }
--- backend/auto_project/src/main/java/auto/backe/auto_project/models/Model.java
+++ backend/auto_project/src/main/java/auto/backe/auto_project/models/Model.java
@@ -3,8 +3,16 @@ package auto.backe.auto_project.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Model {
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
@@ -15,6 +23,4 @@ public class Model {
     private int priceRange;
     private int yearOfProduction;
     private boolean isActive;
-
-
 }

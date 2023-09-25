c39d92486fc6b7159a8dccd2e173fa51960a5546
MaxSlouka <jmslouka@gmail.com>
2023-09-25 16:35:33 +0200

    rework implementation

--- backend/auto_project/src/main/java/services/implementation/ModerServiceImpl.java
+++ backend/auto_project/src/main/java/services/implementation/ModerServiceImpl.java
@@ -4,19 +4,21 @@ import auto.backe.auto_project.models.Model;
 import repo.ModelRepository;
 import services.ModelService;
 
+import java.util.List;
 import java.util.Optional;
 
 public class ModerServiceImpl implements ModelService {
 
     private ModelRepository modelRepository;
+
     @Override
-    public Object getAll() {
+    public List<Model> getAll() {
         return modelRepository.findAll();
     }
 
     @Override
     public Optional<Model> findById(Long id) {
-       return modelRepository.findById(id);
+        return modelRepository.findById(id);
     }
 
     @Override
@@ -25,12 +27,17 @@ public class ModerServiceImpl implements ModelService {
     }
 
     @Override
-    public Object createModel(Model model) {
-        return modelRepository.save(model);
+    public Model createModel(Model model) {
+         return modelRepository.save(model);
     }
 
     @Override
-    public void saveModel(Model existingModel) {
+    public void updateModel(Model existingModel) {
         modelRepository.save(existingModel);
+    }
+
+    @Override
+    public void saveModel(Model model) {
+        modelRepository.save(model);
     }
 }

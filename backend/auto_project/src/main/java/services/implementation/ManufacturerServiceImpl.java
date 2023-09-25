42de453886aa4c778a0a7b53a97be93e849d4688
MaxSlouka <jmslouka@gmail.com>
2023-09-25 16:35:29 +0200

    rework implementation

--- backend/auto_project/src/main/java/services/implementation/ManufacturerServiceImpl.java
+++ backend/auto_project/src/main/java/services/implementation/ManufacturerServiceImpl.java
@@ -4,13 +4,15 @@ import auto.backe.auto_project.models.Manufacturer;
 import repo.ManufacturerRepository;
 import services.ManufacturerService;
 
+import java.util.List;
 import java.util.Optional;
 
 public class ManufacturerServiceImpl implements ManufacturerService {
 
     private ManufacturerRepository manufacturerRepository;
+
     @Override
-    public Object getAll() {
+    public List<Manufacturer> getAll() {
         return manufacturerRepository.findAll();
     }
 
@@ -25,12 +27,17 @@ public class ManufacturerServiceImpl implements ManufacturerService {
     }
 
     @Override
-    public Object createManufacturer(Manufacturer manufacturer) {
+    public Manufacturer createManufacturer(Manufacturer manufacturer) {
         return manufacturerRepository.save(manufacturer);
     }
 
     @Override
-    public void saveManufacturer(Manufacturer existingManufacturer) {
+    public void updateManufacturer(Manufacturer existingManufacturer) {
         manufacturerRepository.save(existingManufacturer);
+    }
+
+    @Override
+    public void saveManufacturer(Manufacturer manufacturer) {
+        manufacturerRepository.save(manufacturer);
     }
 }

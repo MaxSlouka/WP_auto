e69c9962d69d97956f58e20c12f904b95c8d39e3
MaxSlouka <jmslouka@gmail.com>
2023-09-25 14:20:49 +0200

    create delete and find by id for manufacturer service

--- backend/auto_project/src/main/java/services/ManufacturerService.java
+++ backend/auto_project/src/main/java/services/ManufacturerService.java
@@ -1,8 +1,15 @@
 package services;
 
+import auto.backe.auto_project.models.Manufacturer;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public interface ManufacturerService {
     Object getAll();
+
+    Optional<Manufacturer> findById(Long id);
+
+    void deleteManufacturer(Manufacturer manufacturer);
 }

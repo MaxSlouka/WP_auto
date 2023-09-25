96b320f092447c02695e492f1f10516141238b81
MaxSlouka <jmslouka@gmail.com>
2023-09-25 14:12:12 +0200

    create delete car and find car by its id

--- backend/auto_project/src/main/java/services/CarService.java
+++ backend/auto_project/src/main/java/services/CarService.java
@@ -1,8 +1,15 @@
 package services;
 
+import auto.backe.auto_project.models.Car;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public interface CarService {
     Object getAll();
+
+    public void deleteCar(Car car);
+
+    Optional<Car> findCarById(Long id);
 }

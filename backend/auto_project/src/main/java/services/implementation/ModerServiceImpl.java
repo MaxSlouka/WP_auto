b83834a3b04c5a8784fc1dc16d2debd38cd44163
MaxSlouka <jmslouka@gmail.com>
2023-09-25 14:26:18 +0200

    implement delete and find by id for model service

--- backend/auto_project/src/main/java/services/implementation/ModerServiceImpl.java
+++ backend/auto_project/src/main/java/services/implementation/ModerServiceImpl.java
@@ -1,7 +1,10 @@
 package services.implementation;
 
+import auto.backe.auto_project.models.Model;
 import repo.ModelRepository;
 import services.ModelService;
+
+import java.util.Optional;
 
 public class ModerServiceImpl implements ModelService {
 
@@ -9,5 +12,15 @@ public class ModerServiceImpl implements ModelService {
     @Override
     public Object getAll() {
         return modelRepository.findAll();
+    }
+
+    @Override
+    public Optional<Model> findById(Long id) {
+       return modelRepository.findById(id);
+    }
+
+    @Override
+    public void deleteModel(Model model) {
+        modelRepository.delete(model);
     }
 }

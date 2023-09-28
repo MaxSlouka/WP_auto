cb30508c51f158519dabcb2a50bb049c5f454850
MaxSlouka <jmslouka@gmail.com>
2023-09-28 19:12:31 +0200

    add component scan, add sec. bypass

--- backend/auto_project/src/main/java/auto/backe/auto_project/AutoProjectApplication.java
+++ backend/auto_project/src/main/java/auto/backe/auto_project/AutoProjectApplication.java
@@ -1,13 +1,23 @@
 package auto.backe.auto_project;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@ComponentScan("auto.backe.auto_project.services")
 public class AutoProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AutoProjectApplication.class, args);
+    }
+
+    @Configuration
+    @EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
+    public class SecurityConfig {
     }
 
 }

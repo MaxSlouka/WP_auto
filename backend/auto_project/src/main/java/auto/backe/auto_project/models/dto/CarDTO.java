b10983e041b060373d319b81fb05fa95e6ea7028
MaxSlouka <jmslouka@gmail.com>
2023-09-28 19:11:43 +0200

    add fields

--- backend/auto_project/src/main/java/auto/backe/auto_project/models/dto/CarDTO.java
+++ backend/auto_project/src/main/java/auto/backe/auto_project/models/dto/CarDTO.java
@@ -1,5 +1,6 @@
 package auto.backe.auto_project.models.dto;
 
+import auto.backe.auto_project.models.Model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +9,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CarDTO {
+    private String model;
     private String color;
     private int performance;
     private int consumption;
+    private int yearOfProduction;
     private boolean isActive;
 }
